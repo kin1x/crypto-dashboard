510809abeae5e1852d14343a4a511ec6e44fe82f
FlufflesVIP <saryabov04@mail.ru>
2024-10-22 23:12:29 +0500

    new package and update file header.tsx in folder src

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css'; // Подключение CSS для стилей
+
+const Header: FC = () => (
+    <div className="header">
+        <h1 className="header-title">Crypto Dashboard</h1>
+        <ul className="header-links">
+            <li><Link to="/">Home</Link></li>
+            {/* Если в будущем добавите больше страниц, можете добавить их сюда */}
+        </ul>
+    </div>
+);
+
+export default Header;
